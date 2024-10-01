b524c1221699cba6c9afa4843d6f0899a73459ac
Maxim Svidrak <maximsvid@gmail.com>
2024-10-01 15:52:21 +0200

    CVView new

--- Lebenslauf/LebenslaufApp.swift
+++ Lebenslauf/LebenslaufApp.swift
@@ -14,6 +14,7 @@ struct LebenslaufApp: App {
         
     }
 }
+//sdfsdfasf
 
 #Preview {
     HeaderView(person: Person(image: "person", name: "Maxim Svidrak", education: "Android IOS Deweloper", location: "Berlin", aboutMe: "I am studying at Syntax Institut since the beginning of 2024. I am passionate about native mobile development for Android and iOS."))
--- Lebenslauf/Resume/Subviews/SkillsView.swift
+++ Lebenslauf/Resume/Subviews/SkillsView.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct SkillsView: View {
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 10)
-                .fill(Color.gray.opacity(0.2))
-                .frame(width: 80, height: 80)
-                .shadow(radius: 5)
-                .padding()
+        VStack (alignment: .leading) {
+            Text("Skills")
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.leading)
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.gray.opacity(0.2))
+                                .frame(width: 90, height: 90)
+                                .shadow(radius: 5)
+                                .padding()
+                
+                Image ("figma")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .shadow(radius: 5)
+            }
+            
         }
+        
+        
+        
     }
 }
 
